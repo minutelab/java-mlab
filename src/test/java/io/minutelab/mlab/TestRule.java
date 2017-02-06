ce609bf841db6098bbce305d431c74fff746ef6b
Moshe Litvin <moshe.litvin@gmail.com>
2017-02-06 14:17:35 +0200

    1. Simplified TestRule to just test establishing of a rule for the simplest lab (postgres rule is tested elsewhere) 2. Enable running everything inside a container, and running maven inside the container

--- src/test/java/io/minutelab/mlab/TestRule.java
+++ src/test/java/io/minutelab/mlab/TestRule.java
@@ -14,28 +14,10 @@ import static org.junit.Assert.fail;
 public class TestRule {
 
    @Rule
-   public MlabRule mlab = new MlabRule(ResourcePrepare.filename(this,"/postgres.mlab"),
-                                       "-schema",
-                                       ResourcePrepare.filename(this,"/test.sql"),"-port","0");
+   public MlabRule mlab = new MlabRule(ResourcePrepare.filename(this,"/tst.mlab"));
 
    @Test
-   public void testDB() throws Exception {
-       Integer port = mlab.lab.getPort();
-       System.out.println("testDB using port "+port);
-       Connection con = DriverManager.getConnection("jdbc:postgresql://localhost:"+port+"/postgres", "postgres", "");
-       Statement st = con.createStatement();
-       ResultSet rs = st.executeQuery("SELECT VERSION()");
-
-       if (rs.next()) {
-           System.out.println("Database version: " + rs.getString(1));
-       } else {
-           fail("Could not find version");
-       }
-
-       rs = st.executeQuery("SELECT val FROM test where id = 1");
-       if (!rs.next()) {
-           fail("Could not find data in database");
-       }
-       assertEquals("Should get the right value",rs.getString(1),"test");
+   public void testLab() throws Exception {
+       System.out.println("Starting test (the lab should be running)");
    }
 }
